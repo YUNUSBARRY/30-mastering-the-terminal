303676e81723a896486f0bd9ad4ae629be904565
yunus barry <yunusbarry39@outlook.com>
2025-04-06 21:39:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// // Import the Express library
+// const express = require("express");
+// const app = express();
+
+// // Define the port for the server to listen on
+// const PORT = 3000;
+
+// // Set up a route for the root URL
+// app.get("/", (req, res) => {
+//   res.send("Hello, World! This is your server speaking. Barry hear's you");
+// });
+
+// // Start the server and listen for incoming requests
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+class Pet {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    eat() {
+      const { name, age } = this;
+      return `My name is ${name} and I am ${age} years old`;
+    }
+  }
+  
+  class Cat extends Pet {
+    constructor(name, age, owner) {
+      super(name, age);
+      this.owner = owner;
+    }
+    meow() {
+      return `${'Meow '.repeat(3)} ${this.owner} is my owner`
+    }
+  }
+  
+  class Dog extends Pet {
+    bark() {
+      return "Wooh!!!!  ".repeat(3);
+    }
+  }
+  
+  const dogTalk = new Dog("Tilson", 14);
+  const catTalk = new Cat("Tilson", 14, 'Yunus Barry');
+  
+  console.log(dogTalk.bark());
+  console.log(catTalk.meow());  
